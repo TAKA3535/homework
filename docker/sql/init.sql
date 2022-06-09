c25b9b5b5ecf64a48748bca79765323ede2ec3b2
takun <neithi.gensin@gmail.com>
2022-06-10 03:18:52 +0900

    a

--- docker/sql/init.sql
+++ docker/sql/init.sql
@@ -14,8 +14,8 @@ INSERT INTO `homework.todos` (
 ) VALUES
   (1, 'hajime takeuchi', 'super hoge master'),
   (2, 'kou matsumoto', '本を買う'),
-  (3, 'masato sasho' , 'ORANGE RANGE21周年ライブに行く'),
+  (3, 'masato sasho', 'ORANGE RANGE21周年ライブに行く'),
   (4, 'takumi koyama', 'スーパーイケメンボーイ'),
-  (5, 'yasuo kouzai' , '最近パソコン買い換えた'),
+  (5, 'yasuo kouzai', '最近パソコン買い換えた'),
   (6, 'takahiro mori', 'MacBook Air買う')
 ;
