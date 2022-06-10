9cd70eced3c1a73916c274779cd3de4fae0abdd3
takun <neithi.gensin@gmail.com>
2022-06-10 12:24:36 +0900

    aaa

--- src/app.js
+++ src/app.js
@@ -5,3 +5,8 @@ const app = express();
 const server = app.listen(5000, function () {
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
+
+//ブラウザ上とTarendの表示test
+app.get("/api/morita", (req, res, next) => {
+  res.send("表示test");
+});
